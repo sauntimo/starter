d64c25f0f3a8b92baed2395001a18ece88f19740
Tim Saunders <tim@sauntimo.org>
2020-12-23 16:52:40 +0000

    Fixing .gitignore

--- dist/common/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isRegion = exports.regions = void 0;
-exports.regions = ['england-and-wales', 'scotland', 'northern-ireland'];
-const isRegion = (x) => exports.regions.includes(x);
-exports.isRegion = isRegion;
--- dist/shared/Logger.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const jet_logger_1 = tslib_1.__importDefault(require("jet-logger"));
-const logger = new jet_logger_1.default();
-exports.default = logger;
